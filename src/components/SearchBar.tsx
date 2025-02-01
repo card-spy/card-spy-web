325f7a97886a5b03d84b4e1a6a043ada457fc8e2
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-01 13:37:32 +1300

    Stop multiple debounce triggers from single input
    Since the debounce function was being triggered directly, there
    were multiple instances of the debounce function triggering in
    sequence. E.g. for a 4-character query, there were 4 instances of the
    onChange function being called.
    
    By assinging the debounce function to a const, the function reference
    appears to be preserved which means that the onChange only gets called
    once per debounced input.
    
    Maybe. Probably. Entering text into the search input doesn't cause the
    component to re-render, so there's only a single function reference
    returned by the call to debounce. Previously, every invocation of
    handleChange created a new function because it called debounce and
    invoked directly.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,14 +3,18 @@
 import { debounce } from "radash";
 import { type FC } from "react";
 
+const INPUT_DELAY = 300;
+
 export interface SearchBarProps {
     onChange?: (value: string) => void;
 }
 
 export const SearchBar: FC<SearchBarProps> = ({ onChange }) => {
 
+    const debounceChange = debounce({ delay: INPUT_DELAY }, (value) => value && onChange?.(value));
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        debounce({ delay: 300 }, () => e.target.value && onChange?.(e.target.value))();
+        debounceChange(e.target.value);
     }
 
     return <input className="font-[family-name:var(--font-geist-mono)] text-white bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded border border-solid border-black/[.08] dark:border-white/[.145] text-lg w-full h-12" type="text" onChange={handleChange} />
