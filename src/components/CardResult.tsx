4a43ad6a969e351cede9190d4ac6206722d7280e
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-03 18:38:31 +1300

    Add white border on hover

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -8,7 +8,7 @@ interface CardResultProps {
 
 export const CardResult: FC<CardResultProps> = ({ card }) => {
     return (
-        <div className="flex flex-col gap-2 border border-solid border-black/[.08] dark:border-white/[.145] rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] w-full">
+        <div className="flex flex-col gap-2 border border-solid border-black/[.08] dark:border-white/[.145] hover:border-white rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] w-full">
             <div className="flex gap-2 items-center">
                 <Image
                     src={card.image}
