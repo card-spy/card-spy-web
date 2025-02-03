1bb2aa33289bf36559d89f165b867e512f646867
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-03 18:38:24 +1300

    Add placeholder text for search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -16,5 +16,10 @@ export const SearchBar: FC<SearchBarProps> = ({ onChange }) => {
         debounceChange(e.target.value);
     }
 
-    return <input className="font-[family-name:var(--font-geist-mono)] text-white bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded border border-solid border-black/[.08] dark:border-white/[.145] text-lg w-full h-12" type="text" onChange={handleChange} />
+    return <input
+        className="font-[family-name:var(--font-geist-mono)] text-white bg-black/[.05] dark:bg-white/[.06] px-2 py-0.5 rounded border border-solid border-black/[.08] dark:border-white/[.145] text-lg w-full h-12"
+        type="text"
+        onChange={handleChange}
+        placeholder="Search for a card..."
+    />
 }
