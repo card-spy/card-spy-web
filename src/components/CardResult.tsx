72d6cd30fe3a566ace5fc2eef56f70f819253918
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-07 22:14:55 +1300

    Add fallback image handling

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -1,22 +1,43 @@
 import Image from 'next/image';
 import { Card } from '@/api/getCards';
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 
 interface CardResultProps {
   card: Card;
 }
 
+const FallbackCard: FC = () => (
+  <div className='flex justify-center items-center h-24 w-[69px] border border-solid border-gray-300'>
+    <Image
+      aria-hidden
+      src="/planeswalker.svg"
+      alt="Planeswalker Symbol / MTG Logo"
+      width={64}
+      height={64}
+    />
+  </div>
+)
+
 export const CardResult: FC<CardResultProps> = ({ card }) => {
+
+  const [imageHasError, setImageHasError] = useState(false);
+
+  if (!imageHasError && card.image.trim().length === 0) {
+    setImageHasError(true);
+  }
+
   return (
     <div className="flex flex-col gap-2 border border-solid border-black/[.08] dark:border-white/[.145] hover:border-white rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] w-full">
       <div className="flex gap-2 items-center">
-        <Image
-          src={card.image}
-          alt={card.name}
-          height={96}
-          width={69}
-          className="rounded-lg"
-        />
+        {imageHasError ? <FallbackCard /> :
+          (<Image
+            src={card.image}
+            alt={card.name}
+            height={96}
+            width={69}
+            className="rounded-lg"
+            onError={() => setImageHasError(true)}
+          />)}
         <div className="flex flex-col">
           <div className="text-lg font-bold">{card.name}</div>
           <div className="text-sm text-gray-500">{card.set}</div>
