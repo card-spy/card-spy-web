06ee2e8f774057c7ee5647406d97f7ebbd8bbf24
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-08 19:58:27 +1300

    Make price more prominent in card result, add link

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -26,8 +26,13 @@ export const CardResult: FC<CardResultProps> = ({ card }) => {
   }
 
   return (
-    <div className='flex flex-col gap-2 items-start justify-center w-full min-h-32 border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-white rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] hover:bg-black transition-colors duration-300'>
-      <div className='flex gap-2 items-center'>
+    <a
+      href='https://github.com/card-spy/card-spy-web'
+      target='_blank'
+      rel='noopener noreferrer'
+      className='flex flex-row items-start justify-between w-full min-h-24 border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-white rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] hover:bg-black transition-colors duration-300'
+    >
+      <div className='flex flex-row gap-2 items-center'>
         {imageHasError ? (
           <FallbackCard />
         ) : (
@@ -40,13 +45,15 @@ export const CardResult: FC<CardResultProps> = ({ card }) => {
             onError={() => setImageHasError(true)}
           />
         )}
-        <div className='flex flex-col'>
+        <div className='flex flex-col items-left'>
           <div className='text-lg font-bold'>{card.name}</div>
           <div className='text-sm text-gray-500'>{card.set}</div>
-          <div className='text-xs text-gray-500'>{card.price}</div>
           <div className='text-sm italic text-gray-400'>{card.store}</div>
         </div>
       </div>
-    </div>
+      <div className='flex flex-col gap-2 items-end w-fit'>
+        <div className='text-xl text-gray-100'>{card.price}</div>
+      </div>
+    </a>
   );
 };
