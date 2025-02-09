437a3a725fd48185ad9744370dab53b551c0737f
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-09 15:30:22 +1300

    Update CSS classes to support light mode

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -25,7 +25,7 @@ export const CardResult: FC<CardResultProps> = ({ card }) => {
       href={card.storeLink}
       target='_blank'
       rel='noopener noreferrer'
-      className='flex flex-row items-start justify-between w-full min-h-24 border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-white rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] hover:bg-black transition-colors duration-300'
+      className='flex flex-row items-start justify-between w-full min-h-24 border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-black dark:hover:border-white rounded-lg p-4 bg-black/[.05] dark:bg-white/[.06] hover:bg-gray-50 dark:hover:bg-black transition-colors duration-300'
     >
       <div className='flex flex-row gap-2 items-center'>
         {imageHasError ? (
@@ -53,7 +53,7 @@ export const CardResult: FC<CardResultProps> = ({ card }) => {
         </div>
       </div>
       <div className='flex flex-col gap-2 items-end w-fit'>
-        <div className='text-xl text-gray-100'>{card.price}</div>
+        <div className='text-xl dark:text-gray-100'>{card.price}</div>
       </div>
     </a>
   );
