af150c2bf879e3be189d68b3dc86dae132f503bf
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-10 07:11:56 +1300

    Autofocus search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -31,6 +31,7 @@ export const SearchBar: FC<SearchBarProps> = ({
 
   return (
     <input
+      autoFocus
       className='font-[family-name:var(--font-geist-mono)] text-black dark:text-white bg-black/[.05] dark:bg-white/[.06] px-2 py-0.5 rounded-lg border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-black dark:hover:border-white hover:focus:border-black/[.08] text-lg w-full h-12'
       type='text'
       onChange={handleChange}
