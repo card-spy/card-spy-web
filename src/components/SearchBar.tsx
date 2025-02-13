daf1d8d0c759a13033e992c3d6efd7385169d82f
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-13 23:51:49 +1300

    Add search icon to search input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Icon } from '@iconify/react';
 import { debounce } from 'radash';
 import { type FC } from 'react';
 
@@ -30,12 +31,20 @@ export const SearchBar: FC<SearchBarProps> = ({
   };
 
   return (
-    <input
-      autoFocus
-      className='font-[family-name:var(--font-geist-mono)] text-black dark:text-white bg-black/[.05] dark:bg-white/[.06] px-2 py-0.5 rounded-lg border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-black dark:hover:border-white hover:focus:border-black/[.08] text-lg w-full h-12'
-      type='text'
-      onChange={handleChange}
-      placeholder='Search for a card...'
-    />
+    <div className='relative'>
+      <Icon
+        className='absolute top-3 left-2'
+        icon='material-symbols:search-rounded'
+        width='24'
+        height='24'
+      />
+      <input
+        autoFocus
+        className='font-[family-name:var(--font-geist-mono)] text-black dark:text-white bg-black/[.05] dark:bg-white/[.06] px-9 py-0.5 rounded-lg border-2 border-solid border-black/[.08] dark:border-white/[.145] hover:border-black dark:hover:border-white hover:focus:border-black/[.08] text-lg w-full h-12'
+        type='text'
+        onChange={handleChange}
+        placeholder='Search for a card...'
+      />
+    </div>
   );
 };
