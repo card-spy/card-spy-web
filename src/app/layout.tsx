e97d3929d857c1cc4195556aee90de0cdd3f6623
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-02-19 22:36:14 +1300

    Fix search plugin link
    After revising the Next docs, the recommendation for app router is to
    just put the link directly in Layout without using the Head component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,15 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      {/* Next App Router doesn't currently support custom link tags in metadata */}
-      <Head>
-        <link
-          rel='search'
-          type='application/opensearchdescription+xml'
-          href='https://cards.matteas.nz/opensearch.xml'
-          title='Card Spy'
-        />
-      </Head>
+      <link
+        rel='search'
+        type='application/opensearchdescription+xml'
+        href='https://cards.matteas.nz/opensearch.xml'
+        title='Card Spy'
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
